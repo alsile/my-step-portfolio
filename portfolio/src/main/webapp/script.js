1d62e5b6fc9832a25e21a608fb069d0e03d14a88
Eyad Alsilimy <alsile@uw.edu>
2020-07-01 20:44:37 +0000

    Add fetching to portfolio and information to examples

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -66,7 +66,7 @@ function setDarkMode() {
   if (localStorage.getItem("darkOn") === "true") {
     document.body.classList.add("dark-mode");
   } else {
-     document.body.classList.remove("dark-mode");
+    document.body.classList.remove("dark-mode");
   }
 }
 
@@ -77,4 +77,10 @@ function toggleDarkMode() {
     localStorage.darkOn = "true";
   }
   setDarkMode(localStorage.getItem("darkOn"));
+}
+
+async function pageViews() {
+  const response = await fetch('/data');
+  const quote = await response.text();
+  document.getElementById('page-views').innerText = quote;
 }
