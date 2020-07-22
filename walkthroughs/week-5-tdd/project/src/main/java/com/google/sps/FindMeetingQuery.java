0b17fc2fd65b24ccfae6f64e575dc76faa0e2e83
Eyad Alsilimy <alsile@uw.edu>
2020-07-22 00:24:14 +0000

    Revert "complete query method that passes all provided tests"
    This reverts commit cbc23ac93625ec86bf00cae1f23f1934be447582.

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,55 +14,10 @@
 
 package com.google.sps;
 
-import java.util.Comparator;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.ArrayList;
-import java.util.List;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    Collection<String> attendees = request.getAttendees();
-    List<TimeRange> unavailibleTimes = new ArrayList<>();
-    for (Event e : events) {
-        if (!Collections.disjoint(e.getAttendees(), attendees)) {
-            unavailibleTimes.add(e.getWhen());
-        }
-    }
-    Collections.sort(unavailibleTimes, TimeRange.ORDER_BY_START_THEN_END);
-    for (int i = 0; i < unavailibleTimes.size() - 1; i++) {
-        while (i < unavailibleTimes.size() - 1 && unavailibleTimes.get(i).contains(unavailibleTimes.get(i+1))) {
-            unavailibleTimes.remove(i+1);
-        }
-        while (i < unavailibleTimes.size() - 1 && unavailibleTimes.get(i).overlaps(unavailibleTimes.get(i+1))) {
-            int addDuration = Math.abs(unavailibleTimes.get(i).end() - unavailibleTimes.get(i+1).end());
-            unavailibleTimes.set(i, TimeRange.fromStartDuration(unavailibleTimes.get(i).start(), unavailibleTimes.get(i).duration() + addDuration));
-            unavailibleTimes.remove(i+1);
-        }
-    }
-    List<TimeRange> availibleTimes = new ArrayList<>();
-    long meetingDuration = request.getDuration();
-    if (unavailibleTimes.size() == 0) {
-        if (meetingDuration <= TimeRange.WHOLE_DAY.duration()) {
-            availibleTimes.add(TimeRange.WHOLE_DAY);
-        }
-    } else {
-        TimeRange first = TimeRange.fromStartEnd(TimeRange.START_OF_DAY, unavailibleTimes.get(0).start(), false);
-        if (first.duration() >= meetingDuration) {
-            availibleTimes.add(first);
-        }
-        for (int i = 0; i < unavailibleTimes.size() - 1; i++) {
-            TimeRange ofI = TimeRange.fromStartEnd(unavailibleTimes.get(i).end(), unavailibleTimes.get(i+1).start(), false);
-            if (ofI.duration() >= meetingDuration) {
-                availibleTimes.add(ofI);
-            }
-        }
-        TimeRange last = TimeRange.fromStartEnd(unavailibleTimes.get(unavailibleTimes.size()-1).end(), TimeRange.END_OF_DAY, true);
-        if (last.duration() >= meetingDuration) {
-            availibleTimes.add(last);
-        }
-    }
-    return availibleTimes;
-    // throw new UnsupportedOperationException("TODO: Implement this method.");
+    throw new UnsupportedOperationException("TODO: Implement this method.");
   }
 }
